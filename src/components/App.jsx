d5f79ac959935df48a27e309049f00f811b6a735
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-01-07 19:24:54 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,14 +8,20 @@ import styles  from './App.module.css';
 
 export class App  extends Component {
   state = {
-    contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
+    contacts: [],
     filter: '',        
   };
+
+  componentDidMount() {    
+    const contacts = JSON.parse(localStorage.getItem('contacts')) || this.state.contacts;
+    this.setState({ contacts });
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts.length !== 0 && prevState.contacts.length !== this.state.contacts.length){    
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }    
+  }
 
   isContactInState = ({ name }) =>
     !!this.state.contacts.filter(({name: prevName}) => {
